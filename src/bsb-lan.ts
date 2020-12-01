d0db557ac48af4872419bc830b62e01037dd122a
Konrad Mattheis <konne@users.noreply.github.com>
2020-12-01 16:56:15 +0100

    * add functionality for fetch of data

--- src/bsb-lan.ts
+++ src/bsb-lan.ts
@@ -6,9 +6,19 @@ export = function (RED: NodeAPI) {
     function NodeBsbLan(this: Node, config: any) {
         RED.nodes.createNode(this, config);
 
+        let node = this;
         let device = RED.nodes.getNode(config.device) as BSBLanDeviceNode;
 
         this.on('input', function (msg: NodeMessageInFlow & { }) {
+           device.get('JQ='+ config.values.join(','))
+            .then((result) => { 
+                msg.payload = result; 
+                node.send(msg);
+            })
+            .catch((error)=>{
+                msg.payload = 'error '+ error;
+                node.send(msg);
+            });
            
         });
 
